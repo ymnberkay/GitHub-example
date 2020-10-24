94466423c89803c99919d729e2caa99f8de82df1
berkay <200201003@kocaeli.edu.tr>
2020-10-24 13:41:58 +0300

    Initial Commit

---
+++ berkaydenemeyine/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  berkaydenemeyine
+//
+//  Created by Berkay Yaman on 24.10.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
