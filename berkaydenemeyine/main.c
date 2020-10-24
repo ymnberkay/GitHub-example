7bec2e0880c23a4c16c6208faad0354b685aa7bb
berkay <200201003@kocaeli.edu.tr>
2020-10-24 14:12:34 +0300

    removed to comment source code

--- berkaydenemeyine/main.c
+++ berkaydenemeyine/main.c
@@ -1,14 +1,10 @@
-//
-//  main.c
-//  berkaydenemeyine
-//
-//  Created by Berkay Yaman on 24.10.2020.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    float a,b,c,x;
+        printf("Lutfen a,b,c,x degerlerini sirasiyla giriniz \n");
+           scanf("%f%f%f%f",&a,&b,&c,&x);
+    float sonuc=a*x*x+b*x+c ;
+    printf("DEGERI : %f",sonuc);
     return 0;
 }
